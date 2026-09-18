0c6fad092fb7071cebb6b766eab2114d163b19bc
Emilie <emiliedumont29@gmail.com>
2026-09-18 12:17:49 +0200

    toggle completed d'une tâche

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,6 +1,7 @@
 //export default veut juste dire "ce fichier peut être importé ailleurs"
 /*Todo.js = une fiche dans un carnet d'adresses. Elle concerne une seule tâche. Tout ce qui décrit ou modifie cette tâche précise (ses propriétés, cocher/décocher elle-même, s'afficher elle-même) va ici.*/
 import getTemplate from "./template";
+import DB from "../../DB";
 
 export default class Todo {
   constructor(data) {
@@ -15,6 +16,19 @@ export default class Todo {
     const template = document.createElement("template");
     template.innerHTML = getTemplate(this);
     this.domElt = template.content.firstElementChild;
+    this.initEvents();
     el.append(this.domElt);
+  }
+  async toggleCompleted() {
+    this.completed = !this.completed;
+    this.domElt.classList.toggle("completed");
+    window.TodoList.renderItemsLeftCount();
+    return await DB.updateOne(this);
+  }
+
+  initEvents() {
+    this.domElt.querySelector(".toggle").addEventListener("change", () => {
+      this.toggleCompleted();
+    });
   }
 }
